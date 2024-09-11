89a72049955df4f7583f9b723a64ff285a406b33
Kai Quach <kaiq@iastate.edu>
2024-09-11 14:57:07 -0500

    updated files

--- Experiments/kaiq/unit1_experiments/tutorials-springboot_unit1_2_hellopeople/hellopeople/src/main/java/coms309/WelcomeController.java
+++ Experiments/kaiq/unit1_experiments/tutorials-springboot_unit1_2_hellopeople/hellopeople/src/main/java/coms309/WelcomeController.java
@@ -15,6 +15,10 @@ public class WelcomeController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    @GetMapping("/")
+    public String welcome(){
+        return "Welcome to this test!";
+    }
     @GetMapping("/welcome")
     public Welcome welcome(@RequestParam(value = "name", defaultValue = "User") String name) {
         return new Welcome(counter.incrementAndGet(), String.format(template, name));
