a347ec01cb551e7057182934e4b258848e74c4c3
Eduardo Barboza <Eduardo.BarbozaC@hotmail.com>
2024-09-11 01:19:49 -0500

    modified helloworld to be a silly cat generator for my experiment 1

--- Experiments/ebarboza/SpringbootTestExample/src/main/java/coms309/CatController.java
+++ Experiments/ebarboza/SpringbootTestExample/src/main/java/coms309/CatController.java
@@ -7,19 +7,31 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
-import java.util.List;
+
 
 @RestController
 class CatController {
 
+    /**
+     * list of our cats
+     */
     private ArrayList<Cat> catList = new ArrayList<Cat>();
 
+    /**
+     * Welcome message when you first load the page
+     * @return welcome message
+     */
     @GetMapping("/")
     public String welcome() {
-        return "Welcome to this CatGenerator experiment thingy! Type '/cmd' to get a list of commands";
+        return "Welcome to this CatGenerator experiment thingy! Type a command in the search bar!";
     }
 
-    //when typing into searchbar, page will "react" to, in this case, anything typed
+    /**
+     * Basic method that was present when downloading the tutorial.
+     * Displays "Hello ... lol" along with whatever the user types after '/'.
+     * @param name
+     * @return output string based on input to search bar
+     */
     @GetMapping("/{name}")
     public String welcome(@PathVariable String name) {
         return "Hello wacky test for COMS 309 lol" + name;
@@ -32,7 +44,7 @@ class CatController {
      * @return
      */
     @GetMapping("/cats")
-    public String regCat() {
+    public String printCats() {
         String output = "";
         int catcount = catList.size();
 
@@ -46,15 +58,26 @@ class CatController {
         return output;
     }
 
+    /**
+     * Method for adding new Cat objects to our catlist.
+     * Specifies the name of the cat to be added.
+     * @param name
+     */
     @GetMapping("/add {name}")
     public void add (@PathVariable String name) {
         catList.add(new Cat(name));
     }
 
+    /**
+     * Displays info of the Cat at specified index within our catlist.
+     * @param index
+     * @return cat info
+     */
     @GetMapping("/info/{index}")
     public String getCatInfo(@PathVariable int index)
     {
         if (catList == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        if (index < 0 || index >= catList.size()) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         return catList.get(index).getName() + " " + catList.get(index).getBreed();
     }
 }
