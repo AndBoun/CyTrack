f284f01bf0ceb3debebf0d4a2335dce37e545c75
ecabelin <ecabelin@iastate.edu>
2024-09-12 04:59:05 -0500

    Adding a counter for button countdown

--- Experiments/ecabelin/exp1/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Experiments/ecabelin/exp1/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import java.util.Random;
 
 import org.w3c.dom.Text;
 
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView messageText;   // define message textview variable
     private TextView buttonText;
     private Button button1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,19 +27,42 @@ public class MainActivity extends AppCompatActivity {
 
         // Title Message
         messageText = findViewById(R.id.main_msg_txt);      // link to message textview in the Main activity XML
-        messageText.setText("Welcome Back Chat");
+        messageText.setText("Don't let the counter go down to 0!");
 
         // Button Message
         buttonText = findViewById(R.id.main_button_txt);
-        buttonText.setText("Don't Press This");
-
-        // Button Function
         button1 = findViewById(R.id.main_button_txt);
+
+        CountDownTimer counter = new CountDownTimer(30000, 1000){
+            public long count;
+
+            public void onTick(long millisUntilFinished) {
+                count = millisUntilFinished;
+                buttonText.setText("seconds remaining: " + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                buttonText.setText("done!");
+                this.cancel();
+            }
+
+            public long getCount() {
+                return count;
+            }
+
+            public void setCount(long count) {
+                this.count = count;
+            }
+        };
+
+        counter.start();
+
         button1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
-
+                counter
             }
         });
+
 
 
 
