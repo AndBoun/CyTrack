269457e3528029441e2645cb2b42508d02efb54f
ecabelin <ecabelin@iastate.edu>
2024-09-12 05:31:49 -0500

    Attempting different implementation of counter

--- Experiments/ecabelin/exp1/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Experiments/ecabelin/exp1/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -36,45 +36,46 @@ public class MainActivity extends AppCompatActivity {
         button1 = findViewById(R.id.main_button_txt);
 
         class TimeTracker extends CountDownTimer{
-            private long count;
-            private final long iter;
+            private boolean add;
+
             public TimeTracker(long millisInFuture, long countDownInterval){
                 super(millisInFuture, countDownInterval);
-                count = millisInFuture;
-                iter = countDownInterval;
             }
             public void onTick(long millisUntilFinished) {
+                if (add) {
+                    try {
+
+                        this.wait(10000);
+                    }
+                    catch(Exception e){
+                        return;
+                    }
+
+                    add = false;
+                }
                 buttonText.setText("seconds remaining: " + millisUntilFinished / 1000);
             }
-
             public void onFinish() {
                 buttonText.setText("done!");
                 this.cancel();
             }
 
-            public long getCount() {
-                return count;
+            public void Switch() {
+                add = true;
             }
 
-            public void setCount(long count) {
-                this.count = count;
-            }
 
-            public long getIter(){
-                return iter;
-            }
         }
 
         TimeTracker counter = new TimeTracker(30000, 1000);
 
-        counter.start();
-
         button1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
-                counter.setCount(counter.getCount() + counter.iter);
+                counter.Switch();
             }
         });
 
+        counter.start();
 
 
 
