350214bcb29ff34506282abf1d86ffb5a8a462a9
ecabelin <ecabelin@iastate.edu>
2024-09-12 04:06:06 -0500

    Added a button click event listener loop

--- Experiments/ecabelin/exp1/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
+++ Experiments/ecabelin/exp1/AndroidExample/app/src/main/java/com/example/androidexample/MainActivity.java
@@ -14,18 +14,32 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView messageText;   // define message textview variable
     private TextView buttonText;
-
+    private Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);             // link to Main activity XML
 
         /* initialize UI elements */
+
+        // Title Message
         messageText = findViewById(R.id.main_msg_txt);      // link to message textview in the Main activity XML
         messageText.setText("Welcome Back Chat");
 
+        // Button Message
         buttonText = findViewById(R.id.main_button_txt);
         buttonText.setText("Don't Press This");
+
+        // Button Function
+        button1 = findViewById(R.id.main_button_txt);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+
+            }
+        });
+
+
+
 
     }
 }
