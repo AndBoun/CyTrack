a05e3f263caee436a6b37f2435a4f3f447a4d98e
AndBoun <167598896+AndBoun@users.noreply.github.com>
2024-10-04 12:25:45 -0500

    recover android application from failed merge and deleted branch

---
+++ Frontend/app/src/main/java/com/example/firstscreen/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.firstscreen;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        usernameEditText = findViewById(R.id.Username_field);
+        passwordEditText = findViewById(R.id.Password_field);
+
+        usernameEditText.setOnFocusChangeListener((v, hasFocus) -> {
+            if (hasFocus) {
+                usernameEditText.setHint("");
+            } else {
+                usernameEditText.setHint("Username");
+            }
+        });
+
+        passwordEditText.setOnFocusChangeListener((v, hasFocus) -> {
+            if (hasFocus) {
+                passwordEditText.setHint("");
+            } else {
+                passwordEditText.setHint("Password");
+            }
+        });
+
+        String username = getIntent().getStringExtra("USERNAME");
+        String password = getIntent().getStringExtra("PASSWORD");
+    }
+}
