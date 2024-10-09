544db27dc49e1924790dc1ae9d1e9174b37fbd38
Kai Quach <kaiq@iastate.edu>
2024-10-08 22:12:01 -0500

    UserRepo

--- Backend/Users/src/main/java/Controllers/UserController.java
+++ Backend/Users/src/main/java/Controllers/UserController.java
@@ -1,42 +1,24 @@
 package Controllers;
 import java.util.*;
-
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import Repositories.UserRepository;
 import Entities.User;
-import Respositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/users")
 public class UserController {
-    private String success = "{\"message\":\"success\"}";
-    private String failure = "{\"message\":\"failure\"}";
 
     @Autowired
     UserRepository userRepository;
 
-    @GetMapping(path = "/{id}")
-    public User getUserById(@PathVariable Long id){
-        User result = userRepository.findById(id).get();
 
-        return result;
+    @GetMapping(path = "/user")
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
     }
 
-    @GetMapping(path = "/login")
-    public Object UserAndPassword(@RequestParam("userid") Optional<String> userid,
-                                  @RequestParam("password") Optional<String> password){
-        if(userid.isPresent() && password.isPresent()){
-            Optional<User> user = userRepository.findByUsername(userid.get());
 
-            if (user.isPresent()){
-                if (user.get().getPassword().equals(password.get())){
-                    res.se
-                }
-            }
-        }
-    }
+
 
 
 }
