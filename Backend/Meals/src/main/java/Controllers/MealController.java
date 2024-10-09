8cd167c9665ef9a1952a3f8a6ffa2ca9e1c78b6c
Eduardo Barboza <Eduardo.BarbozaC@hotmail.com>
2024-10-09 03:01:12 -0500

    implemented post request for creating a meal, and a few other small changes

--- Backend/Meals/src/main/java/Controllers/MealController.java
+++ Backend/Meals/src/main/java/Controllers/MealController.java
@@ -5,33 +5,83 @@ import Repositiories.MealRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 public class MealController {
 
     @Autowired
-    MealRepository mealRepository;
-
-    private String success = "{\"message\":\"success\"}";
-    private String failure = "{\"message\":\"failure\"}";
+    MealRepository mealRepo;
 
     /**
-     * return list of all meals in DB
+     * LIST of all meals in DB
      * @return
      */
-    @GetMapping(path = "/meals")
-    List<Meal> getAllMeals() { return mealRepository.findAll(); }
+    @GetMapping("/meals")
+    List<Meal> getAllMeals() { return mealRepo.findAll(); }
 
     /**
-     * return contents of an individual meal
-     * @param id
+     * READ/return contents of an individual meal
+     * @param id of meal to return
      * @return individual meal object
      */
-    @GetMapping(path = "/meal{id}")
-    Meal getMealById(@PathVariable int id) {
-        return mealRepository.findById(id);
+    @GetMapping("/meal{id}")
+    public Map<String, Object> getMealById(@PathVariable Integer id) {
+
+        Map<String, Object> response = new HashMap<>();
+
+        //Find Meal by id
+        Optional<Meal> existingMealOptional = mealRepo.findById(id);
+
+        if (existingMealOptional.isPresent()) {
+            Meal existingMeal = existingMealOptional.get();
+
+            // Return the Meal information and HTTP status code
+            response.put("meal", existingMeal);
+            response.put("status", "200"); // HTTP 200 OK
+        } else {
+            // If Meal does not exist, return a 404 response
+            response.put("message", "Meal not found with id: " + id);
+            response.put("status", "404"); // HTTP 404 Not Found
+        }
+
+        //Return JSON response
+        //SpringBoot automatically converts Java objects (like maps and classes)
+        //into JSON when returned from a controller method!
+        return response;
     }
+
+    /**
+     * CREATE new meal entry
+     * @param meal we're attempting to add/create
+     * @return response indicating whether meal was added or not
+     */
+    @PostMapping("/meal")
+    public Map<String, String> createMeal(@RequestBody Meal meal) {
+
+        Map<String, String> response = new HashMap<>();
+
+        //Check if Meal with same mealName already exists
+        Optional<Meal> existingMeal = mealRepo.findByName(meal.getMealName());
+
+        if (existingMeal.isPresent()) {
+
+            //Meal already exists -- return message indicating this
+            response.put("message", "Meal already exists");
+            response.put("status", "409");
+        } else {
+            mealRepo.save(meal);
+
+            response.put("message", "New Meal posted correctly");
+            response.put("status", "200"); // 200 OK HTTP status
+        }
+
+        return response;
+    }
+
 
 
 
--- Backend/Meals/src/main/java/Entity/Meal.java
+++ Backend/Meals/src/main/java/Entity/Meal.java
@@ -20,22 +20,36 @@ public class Meal {
      */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="meal_id")
     private int mealID;
 
+    @Column(name="meal_name")
     private String mealName;
+
+    @Column(name="calories")
     private int calories;
+
+    @Column(name="protein")
     private int protein;
+
+    @Column(name="carbs")
     private int carbs;
 
     //TODO--figure out best way to store timestamp
+    //TODO--timestamp methods
 
 
+    // =============================== Constructors ================================== //
+
+    public Meal(){}
 
     public Meal(String name, int calories, int protein, int carbs) {
         this.mealName = name;
         this.calories = calories;
         this.protein = protein;
         this.carbs = carbs;
+
+        //TODO initialize/set timestamp
     }
 
     // =============================== Getters and Setters for each field ================================== //
