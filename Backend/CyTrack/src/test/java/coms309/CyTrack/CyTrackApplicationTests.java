9c2b9140f4bb9949a1c60e9554cc74cb9f658675
Kai Quach <kaiquach04@gmail.com>
2024-10-10 09:24:01 -0500

    asgdaewsgfdgdf

---
+++ Backend/CyTrack/src/test/java/coms309/CyTrack/CyTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package coms309.CyTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
