6f81faef36d889df00447bc130dac2e5f3c67476
ecabelin <ecabelin@iastate.edu>
2024-10-10 00:40:07 -0500

    Made final changes to meal_submit.xml formatting

--- Frontend/app/src/main/java/com/example/CyTrack/MealSubmitActivity.java
+++ Frontend/app/src/main/java/com/example/CyTrack/MealSubmitActivity.java
@@ -2,6 +2,7 @@ package com.example.CyTrack;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -9,8 +10,9 @@ import androidx.activity.EdgeToEdge;
 import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MealLogActivity extends AppCompatActivity {
+public class MealSubmitActivity extends AppCompatActivity {
 
+    private EditText MealName, MealCalories, MealProtein, MealCarbs;
     private ImageButton profileSettingsButton, notificationButton, MealsPageButton, LogPageButton;
     private TextView userNameTextView, userStreakTextView;
 
@@ -19,12 +21,36 @@ public class MealLogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.meal_log);
+        setContentView(R.layout.meal_submit);
 
         // Initialize Buttons
         // TOP BUTTONS
         profileSettingsButton = findViewById(R.id.profileSettingsButton);
         notificationButton = findViewById(R.id.notificationButton);
+        // RIGHT MAIN FUNCTION BUTTONS
+        MealsPageButton = findViewById(R.id.MealsPageButton);
+        LogPageButton = findViewById(R.id.LogPageButton);
+
+        // USERINPUT TEXTEDIT
+        MealName = findViewById(R.id.MealName);
+        MealCalories = findViewById(R.id.MealCalories);
+        MealProtein = findViewById(R.id.MealProtein);
+        MealCalories = findViewById(R.id.MealCalories);
+        MealCarbs = findViewById(R.id.MealCarbs);
+
+        // Button Input Listeners
+        MealsPageButton.setOnClickListener(v -> {
+            //TODO: Send to MealPage
+            Intent Navigate = new Intent(MealSubmitActivity.this, MealTrackingMain.class);
+            startActivity(Navigate);
+
+        });
+
+        LogPageButton.setOnClickListener(v -> {
+            //TODO: Send to LogPage
+            Intent Navigate = new Intent(MealSubmitActivity.this, MealLogActivity.class);
+            startActivity(Navigate);
+        });
 
         // Button Input Listeners
         profileSettingsButton.setOnClickListener(v -> {
@@ -33,22 +59,6 @@ public class MealLogActivity extends AppCompatActivity {
 
         notificationButton.setOnClickListener(v -> {
             // Open Notification Activity
-        });
-
-        // RIGHT MAIN FUNCTION BUTTONS
-        MealsPageButton = findViewById(R.id.MealsPageButton);
-        LogPageButton = findViewById(R.id.LogPageButton);
-
-        // Button Input Listeners
-        MealsPageButton.setOnClickListener(v -> {
-            //TODO: Send to MealPage
-            Intent Navigate = new Intent(MealLogActivity.this, MealTrackingMain.class);
-            startActivity(Navigate);
-
-        });
-
-        LogPageButton.setOnClickListener(v -> {
-            //TODO: Send to LogPage
         });
 
         // Handle back press
