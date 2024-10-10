48393df1083bb11523654bc314de40cd833ba818
Kai Quach <kaiquach04@gmail.com>
2024-10-10 09:53:30 -0500

    WORKOUT

--- Backend/CyTrack/src/main/java/CyTrack/Repositories/WorkoutRepository.java
+++ Backend/CyTrack/src/main/java/CyTrack/Repositories/WorkoutRepository.java
@@ -6,8 +6,7 @@ import java.util.Optional;
 
 public interface WorkoutRepository extends JpaRepository<Workout, Long> {
     Optional<Workout> findByWorkoutID(Long workoutID);
-    Optional<Workout> findByWorkoutName(String workoutName);
-    Optional<Workout> findByWorkoutType(String exerciseType);
+    Optional<Workout> findByExerciseType(String exerciseType);
     Optional<Workout> findByUserID(Long userID);
 
 }
