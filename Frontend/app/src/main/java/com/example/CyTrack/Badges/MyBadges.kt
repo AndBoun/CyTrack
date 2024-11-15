fa0a3d78b190fe5a0b17559b931dc17a90e7d589
a2340sr <a2340sr@iastate.edu>
2024-11-15 13:14:43 -0600

    added oncreate documnentation

--- Frontend/app/src/main/java/com/example/CyTrack/Badges/MyBadges.kt
+++ Frontend/app/src/main/java/com/example/CyTrack/Badges/MyBadges.kt
@@ -93,6 +93,12 @@ private val URL = UrlHolder.URL
  * Activity to display badges.
  */
 class BadgesActivity : ComponentActivity() {
+    /**
+     * Called when the activity is starting. This is where most initialization should go.
+     *
+     * @param savedInstanceState If the activity is being re-initialized after previously being shut down
+     *                           then this Bundle contains the data it most recently supplied in onSaveInstanceState(Bundle).
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
