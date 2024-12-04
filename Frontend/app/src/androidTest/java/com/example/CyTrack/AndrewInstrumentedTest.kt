74d220bdca201af50e2de0bf5153b72bdebd0f0a
a2340sr <a2340sr@iastate.edu>
2024-12-04 12:54:18 -0600

    fixed the app name in ExampleInstrumentedTest, and added AndrewIntrumentedTest

---
+++ Frontend/app/src/androidTest/java/com/example/CyTrack/AndrewInstrumentedTest.kt
@@ -0,0 +1,12 @@
+package com.example.CyTrack
+
+import androidx.test.filters.LargeTest
+import androidx.test.runner.AndroidJUnit4
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class AndrewInstrumentedTest {
+
+
+}
