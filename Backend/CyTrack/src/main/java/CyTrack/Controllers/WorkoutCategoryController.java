c0e9cd3041dabadd123a401dbc33965289881605
Kai Quach <kaiq@iastate.edu>
2024-12-11 02:49:43 +0000

    Update 2 files
    - /Backend/CyTrack/src/main/java/CyTrack/Controllers/WorkoutCategoryController.java
    - /Backend/CyTrack/src/main/java/CyTrack/Controllers/MealCategoryController.java

--- Backend/CyTrack/src/main/java/CyTrack/Controllers/WorkoutCategoryController.java
+++ Backend/CyTrack/src/main/java/CyTrack/Controllers/WorkoutCategoryController.java
@@ -12,6 +12,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import CyTrack.Responses.Util.ErrorResponse;
 
 import java.util.List;
 import java.util.Optional;
